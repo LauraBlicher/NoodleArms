058c573b14a0d7c60622c62964522697e5371ea6
Laura Uhliarova <lau.uhl@gmail.com>
2018-11-29 16:09:45 +0100

    noodle,multiplayer,slider

--- NoodleArms/Assets/Scripts_lau/NoodleRight_Movement.cs
+++ NoodleArms/Assets/Scripts_lau/NoodleRight_Movement.cs
@@ -2,7 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Noodle_Movement : MonoBehaviour {
+public class NoodleRight_Movement : MonoBehaviour {
 
     public float speed = 1f;
 
@@ -13,13 +13,13 @@ public class Noodle_Movement : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-        if (Input.GetKey(KeyCode.D))
+        if (Input.GetKey(KeyCode.RightArrow))
             transform.position += new Vector3(speed * Time.deltaTime, 0.0f, 0.0f);
-        if (Input.GetKey(KeyCode.A))
+        if (Input.GetKey(KeyCode.LeftArrow))
             transform.position -= new Vector3(speed * Time.deltaTime, 0.0f, 0.0f);
-        if (Input.GetKey(KeyCode.W))
+        if (Input.GetKey(KeyCode.UpArrow))
             transform.position += new Vector3(0.0f, speed * Time.deltaTime, 0.0f);
-        if (Input.GetKey(KeyCode.S))
+        if (Input.GetKey(KeyCode.DownArrow))
             transform.position -= new Vector3( 0.0f, speed * Time.deltaTime, 0.0f);
     }
 
---
+++ NoodleArms/Assets/Scripts_lau/Scrol_Background.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Scrol_Background : MonoBehaviour {
+
+    public float csrollSpeed = -0.5f;
+    Vector2 startPos;
+
+	// Use this for initialization
+	void Start () {
+        startPos = transform.position;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+        float newPos = Mathf.Repeat(Time.time * csrollSpeed, 20);
+        transform.position = startPos + Vector2.right * newPos;
+
+
+	}
+}
