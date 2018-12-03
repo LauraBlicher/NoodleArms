30d654c10e529b871b7462c467893b89fa14c040
Laura Uhliarova <lau.uhl@gmail.com>
2018-12-03 09:54:48 +0100

    more sprites

--- NoodleArms/Assets/Scripts_lau/NoodleRight_Movement.cs
+++ NoodleArms/Assets/Scripts_lau/NoodleRight_Movement.cs
@@ -2,25 +2,52 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class NoodleRight_Movement : MonoBehaviour {
-
+public class NoodleRight_Movement : MonoBehaviour
+{
+    public enum Hand { Right, Left }
+    public Hand currentHand;
+    Rigidbody2D rbody;
     public float speed = 1f;
 
-	// Use this for initialization
-	void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
-        if (Input.GetKey(KeyCode.RightArrow))
-            transform.position += new Vector3(speed * Time.deltaTime, 0.0f, 0.0f);
-        if (Input.GetKey(KeyCode.LeftArrow))
-            transform.position -= new Vector3(speed * Time.deltaTime, 0.0f, 0.0f);
-        if (Input.GetKey(KeyCode.UpArrow))
-            transform.position += new Vector3(0.0f, speed * Time.deltaTime, 0.0f);
-        if (Input.GetKey(KeyCode.DownArrow))
-            transform.position -= new Vector3( 0.0f, speed * Time.deltaTime, 0.0f);
+    static float handSpeed;
+
+    private string horizontal;
+    private string vertical;
+
+    // Use this for initialization
+    void Start()
+    {
+        currentHand = Hand.Right;
+        rbody = GetComponent<Rigidbody2D>();
+        switch (currentHand){
+            case Hand.Left:
+                horizontal = "Horizontal1";
+                vertical = "Vertical1";
+                break;
+            case Hand.Right:
+                horizontal = "Horizontal2";
+                vertical = "Vertical2";
+                break;
+        }
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        handSpeed = speed;
+        //if (Input.GetKey(KeyCode.RightArrow))
+        //    //transform.position += new Vector3(speed * Time.deltaTime, 0.0f, 0.0f);
+        //     rbody.MovePosition(rbody.position + new Vector2(speed * Time.deltaTime, 0));
+        // if (Input.GetKey(KeyCode.LeftArrow))
+        // transform.position -= new Vector3(speed * Time.deltaTime, 0.0f, 0.0f);
+
+        //if (Input.GetKey(KeyCode.UpArrow))
+        // transform.position += new Vector3(0.0f, speed * Time.deltaTime, 0.0f);
+
+        //if (Input.GetKey(KeyCode.DownArrow))
+        //transform.position -= new Vector3( 0.0f, speed * Time.deltaTime, 0.0f);
+
+        rbody.velocity = new Vector3(Input.GetAxis(horizontal), Input.GetAxis(vertical)) * handSpeed;
     }
 
 }
