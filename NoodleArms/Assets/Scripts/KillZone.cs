6be44d12d0e22bb177615f4dba7486cbc7fc90c3
LauraBlicher <Lblicher@gmail.com>
2018-12-04 12:25:25 +0100

    Screenshake experiments (still not really working)

--- NoodleArms/Assets/Scripts/KillZone.cs
+++ NoodleArms/Assets/Scripts/KillZone.cs
@@ -7,6 +7,9 @@ public class KillZone : MonoBehaviour {
 	public Text countText;
 	public int Topping;
 	public Text loseText;
+	public GameObject cam;
+	private Vector3 camPos;
+	private Quaternion camRot;
 
 	public AudioClip[] audioClip;
 	public AudioSource soundEffects;
@@ -15,6 +18,8 @@ public class KillZone : MonoBehaviour {
 	{
 		SetCountText ();
 		loseText.text = "";
+		camPos = cam.transform.position;
+		camRot = cam.transform.rotation;
 	}
 
 
@@ -25,8 +30,20 @@ public class KillZone : MonoBehaviour {
 			other.gameObject.SetActive(false);
 			Topping = Topping - 1;
 			SetCountText ();
-			soundEffects.PlayOneShot (audioClip [0]);
+			//soundEffects.PlayOneShot (audioClip [0]);
+			StartCoroutine (ScreenShake ());
+			Debug.Log ("Shaketime");
 		}
+	}
+
+	IEnumerator ScreenShake(){
+		cam.GetComponent<Wiggle>().SendMessage("StartShake");
+		Debug.Log ("Start CamShake");
+		yield return new WaitForSeconds (2f);
+		cam.GetComponent<Wiggle> ().SendMessage ("StopShake");
+		Debug.Log ("Stop CamShake");
+		cam.transform.position = camPos;
+		cam.transform.rotation = camRot;
 	}
 
 	public int GetCount()
